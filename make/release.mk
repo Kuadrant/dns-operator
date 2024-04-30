91774bb09086ebe70837d6dbeb6b2762551046c7
Michael Nairn <mnairn@redhat.com>
2024-04-30 14:46:55 +0100

    prepare-release: v0.2.0-rc2
    ```
    make prepare-release IMG_TAG=v0.2.0-rc2 VERSION=0.2.0-rc2 CHANNELS=alpha REPLACES_VERSION=0.1.0
    ```

---
+++ make/release.mk
@@ -0,0 +1,8 @@
+#Release default values
+IMG=quay.io/kuadrant/dns-operator:v0.2.0-rc2
+BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:v0.2.0-rc2
+CATALOG_IMG=quay.io/kuadrant/dns-operator-catalog:v0.2.0-rc2
+CHANNELS=alpha
+BUNDLE_CHANNELS=--channels=alpha
+VERSION=0.2.0-rc2
+REPLACES_VERSION=0.1.0
