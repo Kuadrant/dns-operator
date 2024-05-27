220148b0764b45c233d6bb71ba8c2db5af8c2f67
Phil Brookes <pbrookes@redhat.com>
2024-05-27 12:27:08 +0200

    prepare-release: release-0.3

---
+++ make/release.mk
@@ -0,0 +1,8 @@
+#Release default values
+IMG=quay.io/kuadrant/dns-operator:release-0.3
+BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:release-0.3
+CATALOG_IMG=quay.io/kuadrant/dns-operator-catalog:release-0.3
+CHANNELS=alpha
+BUNDLE_CHANNELS=--channels=alpha
+VERSION=0.3.0-dev
+REPLACES_VERSION=0.2.0
