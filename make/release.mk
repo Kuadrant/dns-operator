f840cbbb33bca3a5ec2f9448dffaf77dc98e6827
dd di cesare <didi@posteo.net>
2024-07-22 17:26:46 +0200

    prepare-release: release-0.4.0-alpha1

---
+++ make/release.mk
@@ -0,0 +1,8 @@
+#Release default values
+IMG=quay.io/kuadrant/dns-operator:v0.4.0-alpha1
+BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:v0.4.0-alpha1
+CATALOG_IMG=quay.io/kuadrant/dns-operator-catalog:v0.4.0-alpha1
+CHANNELS=alpha
+BUNDLE_CHANNELS=--channels=alpha
+VERSION=0.4.0-alpha1
+REPLACES_VERSION=0.3.0
