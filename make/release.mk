a1cc37d47d1980e9b2187587afcdb1cfb2a4bac0
craig <cbrookes@redhat.com>
2024-09-13 10:56:53 +0100

    prepare-release: v0.6.0
    Signed-off-by: craig <cbrookes@redhat.com>

--- make/release.mk
+++ make/release.mk
@@ -1,8 +1,8 @@
 #Release default values
-IMG=quay.io/kuadrant/dns-operator:release-0.6
-BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:release-0.6
-CATALOG_IMG=quay.io/kuadrant/dns-operator-catalog:release-0.6
-CHANNELS=alpha
-BUNDLE_CHANNELS=--channels=alpha
-VERSION=0.6.0-rc1
+IMG=quay.io/kuadrant/dns-operator:v0.6.0
+BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:v0.6.0
+CATALOG_IMG=quay.io/kuadrant/dns-operator-catalog:v0.6.0
+CHANNELS=stable
+BUNDLE_CHANNELS=--channels=stable
+VERSION=0.6.0
 REPLACES_VERSION=0.5.0
