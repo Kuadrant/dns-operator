85fc853edaae4606fa00c109d79ece1ff4f3fdf3
craig <cbrookes@redhat.com>
2024-11-11 11:18:20 +0000

    prepare release v0.8.0
    rh-pre-commit.version: 2.2.0
    rh-pre-commit.check-secrets: ENABLED

--- make/release.mk
+++ make/release.mk
@@ -1,7 +1,7 @@
 #Release default values
-IMG=quay.io/kuadrant/dns-operator:release-0.8
-BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:release-0.8
-CATALOG_IMG=quay.io/kuadrant/dns-operator-catalog:release-0.8
+IMG=quay.io/kuadrant/dns-operator:v0.8.0
+BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:v0.8.0
+CATALOG_IMG=quay.io/kuadrant/dns-operator-catalog:v0.8.0
 CHANNELS=stable
 BUNDLE_CHANNELS=--channels=stable
 VERSION=0.8.0
