a78106c49565d9716181bea5f9ed4110aa921d1b
craig <cbrookes@redhat.com>
2024-11-22 14:27:30 +0000

    prepare release 0.10.0 - again
    Signed-off-by: craig <cbrookes@redhat.com>
    
    rh-pre-commit.version: 2.2.0
    rh-pre-commit.check-secrets: ENABLED

---
+++ make/release.mk
@@ -0,0 +1,8 @@
+#Release default values
+IMG=quay.io/kuadrant/dns-operator:v0.10.0
+BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:v0.10.0
+CATALOG_IMG=quay.io/kuadrant/dns-operator-catalog:v0.10.0
+CHANNELS=stable
+BUNDLE_CHANNELS=--channels=stable
+VERSION=0.10.0
+REPLACES_VERSION=0.9.0
