6ffba110b942101da77c59506e8e89a6728e1b99
dd di cesare <didi@posteo.net>
2024-11-25 14:50:12 +0100

    prepare-release: v0.10.1

--- make/release.mk
+++ make/release.mk
@@ -1,8 +1,8 @@
 #Release default values
-IMG=quay.io/kuadrant/dns-operator:v0.10.0
-BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:v0.10.0
-CATALOG_IMG=quay.io/kuadrant/dns-operator-catalog:v0.10.0
+IMG=quay.io/kuadrant/dns-operator:v0.10.1
+BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:v0.10.1
+CATALOG_IMG=quay.io/kuadrant/dns-operator-catalog:v0.10.1
 CHANNELS=stable
 BUNDLE_CHANNELS=--channels=stable
-VERSION=0.10.0
-REPLACES_VERSION=0.9.0
+VERSION=0.10.1
+REPLACES_VERSION=0.10.0
