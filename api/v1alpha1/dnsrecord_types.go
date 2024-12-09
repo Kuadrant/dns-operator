dae56347454c6fde9fe35d5075cce68f3e990353
Maskym Vavilov <mvavilov@redhat.com>
2024-12-09 10:06:22 +0000

    display healthy cond on get
    Signed-off-by: Maskym Vavilov <mvavilov@redhat.com>

--- api/v1alpha1/dnsrecord_types.go
+++ api/v1alpha1/dnsrecord_types.go
@@ -162,6 +162,7 @@ type DNSRecordStatus struct {
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
 //+kubebuilder:printcolumn:name="Ready",type="string",JSONPath=".status.conditions[?(@.type==\"Ready\")].status",description="DNSRecord ready."
+//+kubebuilder:printcolumn:name="Healthy",type="string",JSONPath=".status.conditions[?(@.type==\"Healthy\")].status",description="DNSRecord healthy."
 
 // DNSRecord is the Schema for the dnsrecords API
 type DNSRecord struct {
