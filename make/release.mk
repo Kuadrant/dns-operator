ac17fdba50a6de099a30e835b0c893c0ba626877
dd di cesare <didi@posteo.net>
2025-02-04 11:05:19 +0100

    prepare-release: v0.13.0-alpha1
    Signed-off-by: dd di cesare <didi@posteo.net>

---
+++ make/release.mk
@@ -0,0 +1,8 @@
+#Release default values
+IMG=quay.io/kuadrant/dns-operator:v0.13.0-alpha1
+BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:v0.13.0-alpha1
+CATALOG_IMG=quay.io/kuadrant/dns-operator-catalog:v0.13.0-alpha1
+CHANNELS=alpha
+BUNDLE_CHANNELS=--channels=alpha
+VERSION=0.13.0-alpha1
+REPLACES_VERSION=0.12.0
