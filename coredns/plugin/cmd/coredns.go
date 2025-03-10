173ceb8a954b93291c260fe570512e3f70ec7366
Michael Nairn <mnairn@redhat.com>
2025-03-10 16:00:03 +0000

    coredns(plugin): Limit plugins enabled in kuadrant CoreDNS
    Limit the number of plugins included by default in the kuadrant build of
    CoreDNS. The plugins enabled are the required ones and more simple
    plugins with few additional dependencies but are useful for
    debug/testing purposes.
    
    The `trace` plugin had licensing issues and was the main reason to
    reduce the plugin imports.
    
    Signed-off-by: Michael Nairn <mnairn@redhat.com>

--- coredns/plugin/cmd/coredns.go
+++ coredns/plugin/cmd/coredns.go
@@ -5,38 +5,16 @@ import (
 
 	"github.com/coredns/caddy"
 	"github.com/coredns/coredns/core/dnsserver"
-	_ "github.com/coredns/coredns/core/plugin"
 	"github.com/coredns/coredns/coremain"
 
 	_ "github.com/kuadrant/coredns-kuadrant"
+	"github.com/kuadrant/coredns-kuadrant/cmd/plugin"
 )
-
-var dropPlugins = map[string]bool{
-	"kubernetes":   true,
-	"k8s_external": true,
-}
 
 const pluginVersion = "0.0.0"
 
-// https://github.com/ori-edge/k8s_gateway/blob/master/cmd/coredns.go
 func init() {
-	var directives []string
-	var alreadyAdded bool
-
-	for _, name := range dnsserver.Directives {
-
-		if dropPlugins[name] {
-			if !alreadyAdded {
-				directives = append(directives, "kuadrant")
-				alreadyAdded = true
-			}
-			continue
-		}
-		directives = append(directives, name)
-	}
-
-	dnsserver.Directives = directives
-
+	dnsserver.Directives = plugin.Directives
 }
 
 func main() {
