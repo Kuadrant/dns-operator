0f2c8ed84f6e9f9b9aeb25f26f3ae7a8fad30864
Michael Nairn <mnairn@redhat.com>
2025-06-17 12:25:57 +0100

    iteration: multicluster-poc
    Use dnsRecord.Status.ZoneDomainName instead of dnsRecord.Spec.RootHost
    for coredns label value.
    
    Signed-off-by: Michael Nairn <mnairn@redhat.com>

--- internal/controller/dnsrecord_controller.go
+++ internal/controller/dnsrecord_controller.go
@@ -639,7 +639,7 @@ func (r *DNSRecordReconciler) applyLabels(ctx context.Context, dnsRecord *v1alph
 				delete(newLabels, provider.CoreDNSRecordZoneLabel)
 			}
 		} else {
-			newLabels[provider.CoreDNSRecordZoneLabel] = dnsRecord.Spec.RootHost
+			newLabels[provider.CoreDNSRecordZoneLabel] = dnsRecord.Status.ZoneDomainName
 		}
 
 		if !maps.Equal(newLabels, dnsRecord.GetLabels()) {
