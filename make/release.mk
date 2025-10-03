a1bfbc8ecc882113d44a0f5de62085674e812d89
Michael Nairn <mnairn@redhat.com>
2025-10-03 10:42:58 +0100

    prepare-release: v0.15.0
    Signed-off-by: Michael Nairn <mnairn@redhat.com>

--- make/release.mk
+++ make/release.mk
@@ -1,8 +1,8 @@
 #Release default values
-IMG=quay.io/kuadrant/dns-operator:release-0.15
-BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:release-0.15
-CATALOG_IMG=quay.io/kuadrant/dns-operator-catalog:release-0.15
-CHANNELS=alpha
-BUNDLE_CHANNELS=--channels=alpha
-VERSION=0.15.0-dev
+IMG=quay.io/kuadrant/dns-operator:v0.15.0
+BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:v0.15.0
+CATALOG_IMG=quay.io/kuadrant/dns-operator-catalog:v0.15.0
+CHANNELS=stable
+BUNDLE_CHANNELS=--channels=stable
+VERSION=0.15.0
 REPLACES_VERSION=0.14.0
