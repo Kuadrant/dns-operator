23f14b16c69ca86d89fa4c6433456de0772bbb78
Jim Fitzpatrick <jfitzpat@redhat.com>
2026-02-05 09:11:44 +0000

    prepare-release: release-0.16
    Signed-off-by: Jim Fitzpatrick <jfitzpat@redhat.com>

---
+++ make/release.mk
@@ -0,0 +1,6 @@
+#Release default values
+IMG=quay.io/kuadrant/dns-operator:release-0.16
+BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:release-0.16
+CATALOG_IMG=quay.io/kuadrant/dns-operator-catalog:release-0.16
+CHANNELS=alpha
+BUNDLE_CHANNELS=--channels=alpha
