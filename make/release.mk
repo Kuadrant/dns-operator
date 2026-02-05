e6edba4b2eb7f54113ec93f266b0c2f1a7acfacb
Jim Fitzpatrick <jfitzpat@redhat.com>
2026-02-05 12:10:58 +0000

    prepare-release: v0.16.0
    Signed-off-by: Jim Fitzpatrick <jfitzpat@redhat.com>

--- make/release.mk
+++ make/release.mk
@@ -1,7 +1,7 @@
 #Release default values
-IMG=quay.io/kuadrant/dns-operator:0.16.0
-BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:0.16.0
-CATALOG_IMG=quay.io/kuadrant/dns-operator-catalog:0.16.0
+IMG=quay.io/kuadrant/dns-operator:v0.16.0
+BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:v0.16.0
+CATALOG_IMG=quay.io/kuadrant/dns-operator-catalog:v0.16.0
 CHANNELS=stable
 BUNDLE_CHANNELS=--channels=stable
 VERSION=0.16.0
