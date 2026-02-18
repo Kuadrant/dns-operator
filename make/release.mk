aabc58c50346fccd2fbbf287971d549541b42b4b
Michael Nairn <mnairn@redhat.com>
2026-02-18 14:00:30 +0000

    chore(coredns): remove externalTrafficPolicy:Local
    Remove externalTrafficPolicy:Local and document EDNS Client Subnet for GeoIP.
    
    Signed-off-by: Michael Nairn <mnairn@redhat.com>

--- make/release.mk
+++ make/release.mk
@@ -1,7 +1,8 @@
 #Release default values
-IMG=quay.io/kuadrant/dns-operator:v0.16.0
-BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:v0.16.0
-CATALOG_IMG=quay.io/kuadrant/dns-operator-catalog:v0.16.0
+IMG=quay.io/kuadrant/dns-operator:v0.16.1
+BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:v0.16.1
+CATALOG_IMG=quay.io/kuadrant/dns-operator-catalog:v0.16.1
+COREDNS_IMG=quay.io/kuadrant/coredns-kuadrant:v0.16.1
 CHANNELS=stable
 BUNDLE_CHANNELS=--channels=stable
-VERSION=0.16.0
+VERSION=0.16.1
