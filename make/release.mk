f8ab610817d00e6f86d4f16f5babeb585808e3f0
Thomas Maas <thomas@webtypes.com>
2026-07-15 10:28:34 +0200

    prepare-release: v0.17.1
    Signed-off-by: Thomas Maas <thomas@webtypes.com>

--- make/release.mk
+++ make/release.mk
@@ -1,8 +1,8 @@
 #Release default values
-IMG=quay.io/kuadrant/dns-operator:v0.17.0
-BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:v0.17.0
-CATALOG_IMG=quay.io/kuadrant/dns-operator-catalog:v0.17.0
-COREDNS_IMG=quay.io/kuadrant/coredns-kuadrant:v0.17.0
+IMG=quay.io/kuadrant/dns-operator:v0.17.1
+BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:v0.17.1
+CATALOG_IMG=quay.io/kuadrant/dns-operator-catalog:v0.17.1
+COREDNS_IMG=quay.io/kuadrant/coredns-kuadrant:v0.17.1
 CHANNELS=stable
 BUNDLE_CHANNELS=--channels=stable
-VERSION=0.17.0
+VERSION=0.17.1
