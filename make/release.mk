986f6f506ad5f4089ef0531fed4d0ffa0f49dc54
Thomas Maas <thomas@webtypes.com>
2026-08-31 10:02:54 +0200

    chore: prepare release v0.17.2
    Signed-off-by: Thomas Maas <thomas@webtypes.com>

--- make/release.mk
+++ make/release.mk
@@ -1,8 +1,8 @@
 #Release default values
-IMG=quay.io/kuadrant/dns-operator:v0.17.1
-BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:v0.17.1
-CATALOG_IMG=quay.io/kuadrant/dns-operator-catalog:v0.17.1
-COREDNS_IMG=quay.io/kuadrant/coredns-kuadrant:v0.17.1
+IMG=quay.io/kuadrant/dns-operator:v0.17.2
+BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:v0.17.2
+CATALOG_IMG=quay.io/kuadrant/dns-operator-catalog:v0.17.2
+COREDNS_IMG=quay.io/kuadrant/coredns-kuadrant:v0.17.2
 CHANNELS=stable
 BUNDLE_CHANNELS=--channels=stable
-VERSION=0.17.1
+VERSION=0.17.2
