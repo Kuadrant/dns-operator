57ba152215166ae0b8b8dce9f13280c7ef6916b2
Michael Nairn <mnairn@redhat.com>
2026-09-25 10:50:00 +0100

    prepare-release: v0.18.0

---
+++ make/release.mk
@@ -0,0 +1,8 @@
+#Release default values
+IMG=quay.io/kuadrant/dns-operator:v0.18.0
+BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:v0.18.0
+CATALOG_IMG=quay.io/kuadrant/dns-operator-catalog:v0.18.0
+COREDNS_IMG=quay.io/kuadrant/coredns-kuadrant:v0.18.0
+CHANNELS=stable
+BUNDLE_CHANNELS=--channels=stable
+VERSION=0.18.0
